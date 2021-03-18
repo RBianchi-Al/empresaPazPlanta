5ee7b4bace6283f61cd4459d755f86d8fb008d57
RBianchi-Al <renatalebianchi@gmail.com>
2021-03-17 22:21:24 -0300

    versao final

---
+++ developer.js
@@ -0,0 +1,10 @@
+
+// function formatar(mascara, documento) {
+//   var i = documento.value.length;
+//   var saida = mascara.substring(0, 1);
+//   var text = mascara.substring(i);
+//   if (text.substring(0, 1) != saida) {
+//     documento.value += text.substring(0, 1);
+//   }
+// }
+
